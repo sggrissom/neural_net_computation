5c0c214a368b3993e0d21ee4a9c7208f758ba103
U-FAA\steve ctr grissom <steve ctr grissom@JAMCXWL00106656.amc.faa.gov>
2014-12-05 11:14:03 -0600

    Fixed the cuda style implementation. CPU now works and GPU scaffolding is in place (although not quite working yet).

--- Cuda Implementation/kernel.cu
+++ Cuda Implementation/kernel.cu
@@ -1,40 +1,167 @@
+
 #include <math.h>
 
 /******************************************************************************
-  GPU kernels
+  GPU main computation kernels
  *******************************************************************************/
 
-__global__ void gpu_naive_kernel(float** in, int len) {
-
-	// INSERT KERNEL CODE HERE
+__global__ void gpu_naive_kernel(double *in,double *tgt,
+		double **out,
+		double **delta,
+		double ***weight,
+		int *numl,
+		int *lsize,
+		double *beta,
+		double *alpha,
+		double*** prevDwt) {
 
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
-	if (idx < len)
-	{
 
+
+	double sum;
+
+	//	update output values for each neuron
+
+
+	//	assign content to input layer
+	if (idx < lsize[0])
+	{
+		out[0][idx]=in[idx];  // output_from_neuron(i,j) Jth neuron in Ith Layer
+	}
+
+
+	__syncthreads();
+
+	//	assign output(activation) value 
+	//	to each neuron usng sigmoid func
+	if (idx < *numl) {
+		for(int j=0;j<lsize[idx];j++){		// For each neuron in current layer
+			sum=0.0;
+			for(int k=0;k<lsize[idx-1];k++){		// For input from each neuron in preceeding layer
+				sum+= out[idx-1][k]*weight[idx][j][k];	// Apply weight to inputs and add to sum
+			}
+			sum+=weight[idx][j][lsize[idx-1]];		// Apply bias
+			out[idx][j]=(double)(1/(1+exp(-sum)));
+		}
+	}
+
+	__syncthreads();
+
+	//	find delta for output layer
+	if (idx < lsize[*numl-1]) {
+		delta[(*numl)-1][idx]=out[(*numl)-1][idx]*(1-out[(*numl)-1][idx])*(tgt[idx]-out[(*numl)-1][idx]);
+	}
+	__syncthreads();
+
+	//	find delta for hidden layers	
+	if (idx < *numl-1 && idx > 0) {
+		for(int j=0;j<lsize[idx];j++){
+			sum=0.0;
+			for(int k=0;k<lsize[idx+1];k++){
+				sum+=delta[idx+1][k]*weight[idx+1][k][j];
+			}
+			delta[idx][j]=out[idx][j]*(1-out[idx][j])*sum;
+		}
+	}
+	__syncthreads();
+
+	//	apply momentum ( does nothing if alpha=0 )
+	if (idx < *numl && idx > 0) {
+		for(int j=0;j<lsize[idx];j++){
+			for(int k=0;k<lsize[idx-1];k++){
+				weight[idx][j][k]+=(*alpha)*prevDwt[idx][j][k];
+			}
+			weight[idx][j][lsize[idx-1]]+=(*alpha)*prevDwt[idx][j][lsize[idx-1]];
+		}
+	}
+	__syncthreads();
+
+	//	adjust weights usng steepest descent	
+	if (idx < *numl && idx > 0) {
+		for(int j=0;j<lsize[idx];j++){
+			for(int k=0;k<lsize[idx-1];k++){
+				prevDwt[idx][j][k]=(*beta)*delta[idx][j]*out[idx-1][k];
+				weight[idx][j][k]+=prevDwt[idx][j][k];
+			}
+			prevDwt[idx][j][lsize[idx-1]]=(*beta)*delta[idx][j];
+			weight[idx][j][lsize[idx-1]]+=prevDwt[idx][j][lsize[idx-1]];
+		}
+	}
+}
+
+__global__ void gpu_improved_kernel(double *in,double *tgt,
+		double **out,
+		double **delta,
+		double ***weight,
+		int *numl,
+		int *lsize,
+		double *beta,
+		double *alpha,
+		double*** prevDwt) {
+
+	int idx = threadIdx.x + blockDim.x * blockIdx.x;
+	if (idx < lsize[0])
+	{
 	}
 }
 
 /******************************************************************************
-  computation functions
+  Main computation functions
  *******************************************************************************/
 
-void cpu_bpgt(float* in,
-		float ans,
-		float** out,
-		float** delta,
-		float*** weight,
-		int numl,
-		int* lsize,
-		float beta,
-		float alpha,
-		float*** prevDwt) {
+void gpu_naive_bpgt(double *in,double *tgt,
+		double **out,
+		double **delta,
+		double ***weight,
+		int *numl,
+		int *lsize,
+		double *beta,
+		double *alpha,
+		double*** prevDwt) {
+
+	const unsigned int numThreadsPerBlock = 512;
+	const unsigned int numBlocks = (128 - 1)/numThreadsPerBlock + 1;
+	gpu_naive_kernel <<< numBlocks , numThreadsPerBlock >>>
+		(in,tgt,out,delta,weight,numl,lsize,beta,alpha,prevDwt);
+}
+
+void gpu_improved_bpgt(double *in,double *tgt,
+		double **out,
+		double **delta,
+		double ***weight,
+		int *numl,
+		int *lsize,
+		double *beta,
+		double *alpha,
+		double*** prevDwt) {
+
+	const unsigned int numThreadsPerBlock = 512;
+	const unsigned int numBlocks = (128 - 1)/numThreadsPerBlock + 1;
+	gpu_improved_kernel <<< numBlocks , numThreadsPerBlock >>>
+		(in,tgt,out,delta,weight,numl,lsize,beta,alpha,prevDwt);
+
+}
 
 
-	float sum;
-	int i,j,k;
+
+//	backpropogate errors from output
+//	layer uptill the first hidden layer
+void cpu_bpgt(double *in,double *tgt,
+		double **out,
+		double **delta,
+		double ***weight,
+		int *numl,
+		int *lsize,
+		double *beta,
+		double *alpha,
+		double*** prevDwt)
+{
+	double sum;
+	int i;
 
 	//	update output values for each neuron
+
+
 	//	assign content to input layer
 	for(i=0;i<lsize[0];i++)
 	{
@@ -43,30 +170,29 @@ void cpu_bpgt(float* in,
 
 	//	assign output(activation) value 
 	//	to each neuron usng sigmoid func
-	for(i=1;i<numl;i++){				// For each layer
-		for(j=0;j<lsize[i];j++){		// For each neuron in current layer
+	for(i=1;i<*numl;i++){				// For each layer
+		for(int j=0;j<lsize[i];j++){		// For each neuron in current layer
 			sum=0.0;
-			for(k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
+			for(int k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
 				sum+= out[i-1][k]*weight[i][j][k];	// Apply weight to inputs and add to sum
 			}
 			sum+=weight[i][j][lsize[i-1]];		// Apply bias
-			out[i][j]= (float) (1/(1+exp(-sum))); 		// Apply sigmoid function
+			out[i][j]=(double)(1/(1+exp(-sum)));
 		}
 	}
 
 
-
 	//	find delta for output layer
-	for(i=0;i<lsize[numl-1];i++){
-		delta[numl-1][i]=out[numl-1][i]*
-			(1-out[numl-1][i])*(ans-out[numl-1][i]);
+	for(i=0;i<lsize[(*numl)-1];i++){
+		delta[(*numl)-1][i]=out[(*numl)-1][i]*
+			(1-out[(*numl)-1][i])*(tgt[i]-out[(*numl)-1][i]);
 	}
 
 	//	find delta for hidden layers	
-	for(i=numl-2;i>0;i--){
-		for(j=0;j<lsize[i];j++){
+	for(i=*numl-2;i>0;i--){
+		for(int j=0;j<lsize[i];j++){
 			sum=0.0;
-			for(k=0;k<lsize[i+1];k++){
+			for(int k=0;k<lsize[i+1];k++){
 				sum+=delta[i+1][k]*weight[i+1][k][j];
 			}
 			delta[i][j]=out[i][j]*(1-out[i][j])*sum;
@@ -74,23 +200,23 @@ void cpu_bpgt(float* in,
 	}
 
 	//	apply momentum ( does nothing if alpha=0 )
-	for(i=1;i<numl;i++){
-		for(j=0;j<lsize[i];j++){
-			for(k=0;k<lsize[i-1];k++){
-				weight[i][j][k]+=alpha*prevDwt[i][j][k];
+	for(i=1;i<*numl;i++){
+		for(int j=0;j<lsize[i];j++){
+			for(int k=0;k<lsize[i-1];k++){
+				weight[i][j][k]+=(*alpha)*prevDwt[i][j][k];
 			}
-			weight[i][j][lsize[i-1]]+=alpha*prevDwt[i][j][lsize[i-1]];
+			weight[i][j][lsize[i-1]]+=(*alpha)*prevDwt[i][j][lsize[i-1]];
 		}
 	}
 
 	//	adjust weights usng steepest descent	
-	for(i=1;i<numl;i++){
-		for(j=0;j<lsize[i];j++){
-			for(k=0;k<lsize[i-1];k++){
-				prevDwt[i][j][k]=beta*delta[i][j]*out[i-1][k];
+	for(i=1;i<*numl;i++){
+		for(int j=0;j<lsize[i];j++){
+			for(int k=0;k<lsize[i-1];k++){
+				prevDwt[i][j][k]=(*beta)*delta[i][j]*out[i-1][k];
 				weight[i][j][k]+=prevDwt[i][j][k];
 			}
-			prevDwt[i][j][lsize[i-1]]=beta*delta[i][j];
+			prevDwt[i][j][lsize[i-1]]=(*beta)*delta[i][j];
 			weight[i][j][lsize[i-1]]+=prevDwt[i][j][lsize[i-1]];
 		}
 	}
@@ -98,14 +224,13 @@ void cpu_bpgt(float* in,
 
 
 // feed forward one set of input
-void cpu_ffwd(float *in,
-		float** out,
-		float*** weight,
-		int numl,
-		int* lsize
-		)
+void ffwd(double *in,
+		double **out,
+		double ***weight,
+		int *numl,
+		int *lsize)
 {
-	float sum;
+	double sum;
 	int i=0;
 
 	//	assign content to input layer
@@ -116,24 +241,17 @@ void cpu_ffwd(float *in,
 
 	//	assign output(activation) value 
 	//	to each neuron usng sigmoid func
-	for(i=1;i<numl;i++){				// For each layer
+	for(i=1;i<*numl;i++){				// For each layer
 		for(int j=0;j<lsize[i];j++){		// For each neuron in current layer
 			sum=0.0;
 			for(int k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
 				sum+= out[i-1][k]*weight[i][j][k];	// Apply weight to inputs and add to sum
 			}
 			sum+=weight[i][j][lsize[i-1]];		// Apply bias
-			out[i][j]= (1/(1+exp(-sum)));// Apply sigmoid function
+			out[i][j]=(double)(1/(1+exp(-sum)));
 		}
 	}
 }
 
-float cpu_mse(float *tgt, float **out, int* lsize, int numl)
-{
-	float mse=0;
-	for(int i=0;i<lsize[numl-1];i++){
-		mse+=(tgt[i]-out[numl-1][i])*(tgt[i]-out[numl-1][i]);
-	}
-	return mse/2;
-}
+
 
--- Cuda Implementation/main.cu
+++ Cuda Implementation/main.cu
@@ -1,139 +1,171 @@
 
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+// NeuralNet.cpp : Defines the entry point for the console application.
 
-#include <stdio.h>
-
-#include "support.h"
 #include "kernel.cu"
+#include "support.h"
+
+using std::cout;
+using std::endl;
 
 int main(int argc, char* argv[])
 {
-	Timer timer;
+	printf("Let's begin");
 
-	float data[]={
-		0,	0,	0,	0,
-		0,	0,	1,	1,
-		0,	1,	0,	1,
-		0,	1,	1,	0,
-		1,	0,	0,	1,
-		1,	0,	1,	0,
-		1,	1,	0,	0,
-		1,	1,	1,	1 };
-
-	int inputSize = 3;
-	int dataPoints = 8;
-	float beta = 0.3;
-	float alpha = 0.1;
-	float thresh = 0.1;
-	int maxIterations = 100000000;
-	
-	// Initialize host variables ----------------------------------------------
-	float** out;
-	float** delta;
-	float*** weight;
-	int* lsize;
-	float*** prevDwt;
-
-	cudaError_t cuda_ret;
-
-	// Constants
-	const int numl = 4;
-	const int outputSize = 1;
-
-	enum Mode {CPU_NORMAL = 1, GPU_NAIVE, GPU_IMPROVED };
+	enum Mode {CPU_NORMAL = 1, GPU_NAIVE, GPU_IMPROVED};
 	Mode mode;
-	enum DataSet {XOR = 1, MUSHROOM };
-	DataSet dataSrc;
+	enum DataSet {XOR = 1};
+	DataSet dataSet;
 
-	if(argc == 2) {
+	if(argc == 1) {
+		mode = CPU_NORMAL;
+		dataSet = XOR;
+	} else if(argc == 2) {
 		mode = (Mode) atoi(argv[1]);
-		dataSrc = (DataSet) XOR;
+		dataSet = XOR; 
 	} else if(argc == 3) {
 		mode = (Mode) atoi(argv[1]);
-		dataSrc = (DataSet) atoi(argv[2]);
-	} else {
+		dataSet = (DataSet) atoi(argv[2]);
+	}else {
 		printf("\n    Invalid input parameters."
-				"\n"
-				"\n    Modes: 1 = CPU normal execution"
-				"\n           2 = GPU normal execution"
-				"\n           3 = GPU with fancy"
 				"\n\n");
 		exit(0);
 	}
 
-	if (dataSrc == XOR) {
-	;	
-	}
-
+	Timer timer;
+	cudaError_t cuda_ret;
 	printf("\nSetting up the problem..."); fflush(stdout);
 	startTime(&timer);
 
-	lsize= (int*) malloc(numl * sizeof(int));
+	int inputSize = 3;
+	int outputSize = 1;
+	int dataPoints = 8;
+	int testPoints = 8;
 
-	int i, j, k;
+	double*data;
+	double *data_d;
 
-	for(i=0;i<numl;i++){
-		if (i == numl-1)
-			lsize[i] = outputSize;
-		else
-			lsize[i] = inputSize-1;
+	if(dataSet==XOR) {
+		double xorData[]={
+			0,	0,	0,	0,
+			0,	0,	1,	1,
+			0,	1,	0,	1,
+			0,	1,	1,	0,
+			1,	0,	0,	1,
+			1,	0,	1,	0,
+			1,	1,	0,	0,
+			1,	1,	1,	1 };
+
+		data = xorData;
 	}
 
-	//	allocate memory for output of each neuron
-	out = (float**) malloc(numl * sizeof(float*));
+	int sz[4] = {inputSize,
+		inputSize-1,
+		inputSize-2,
+		outputSize};
+
+	int i;
+
+	double beta = 0.3, alpha = 0.1;
+	double *beta_d;
+	double *alpha_d;
+	long num_iter = 2000000;
+
+	double **out;
+	double **out_d;
+	double **delta;
+	double **delta_d;
+	double ***weight;
+	double ***weight_d;
+	int numl=4;
+	int*numl_d;
+	int *lsize = new int[numl];
+	int *lsize_d;
+	for(i=0;i<numl;i++){
+		lsize[i]=sz[i];
+	}
+	double ***prevDwt;
+	double ***prevDwt_d;
+
+	out = new double*[numl];
 
 	for( i=0;i<numl;i++){
-		out[i]= (float*) malloc(lsize[i] * sizeof(float));
+		out[i]=new double[lsize[i]];
 	}
 
 	//	allocate memory for delta
-	delta = (float**) malloc(numl * sizeof(float*));
+	delta = new double*[numl];
 
-		for(i=1;i<numl;i++){
-			delta[i]=(float*) malloc(lsize[i] * sizeof(float));
-		}
+	for(i=1;i<numl;i++){
+		delta[i]=new double[lsize[i]];
+	}
 
 	//	allocate memory for weights
-	weight = (float***) malloc(numl * sizeof(float**));
+	weight = new double**[numl];
 
-		for(i=1;i<numl;i++){
-			weight[i]=(float**) malloc(lsize[i] * sizeof(float*));
-			for(j=0;j<lsize[i];j++){
-				weight[i][j]=(float*) malloc(lsize[i] * sizeof(float));
-			}
+	for(i=1;i<numl;i++){
+		weight[i]=new double*[lsize[i]];
+	}
+	for(i=1;i<numl;i++){
+		for(int j=0;j<lsize[i];j++){
+			weight[i][j]=new double[lsize[i-1]+1];
 		}
+	}
 
 	//	allocate memory for previous weights
-	prevDwt = (float***) malloc(numl * sizeof(float**));
+	prevDwt = new double**[numl];
 
-		for(i=1;i<numl;i++){
-			prevDwt[i]=(float**) malloc(lsize[i] * sizeof(float*));
-			for(j=0;j<lsize[i];j++){
-				prevDwt[i][j]=(float*) malloc(lsize[i] * sizeof(float));
-			}
+	for(i=1;i<numl;i++){
+		prevDwt[i]=new double*[lsize[i]];
+
+	}
+	for(i=1;i<numl;i++){
+		for(int j=0;j<lsize[i];j++){
+			prevDwt[i][j]=new double[lsize[i-1]+1];
 		}
+	}
 
 	//	seed and assign random weights
 	srand((unsigned)(time(NULL)));
 	for(i=1;i<numl;i++)
-		for(j=0;j<lsize[i];j++)
-			for(k=0;k<lsize[i-1]+1;k++)
+		for(int j=0;j<lsize[i];j++)
+			for(int k=0;k<lsize[i-1]+1;k++)
 				weight[i][j][k]=(double)(rand())/(RAND_MAX/2) - 1;//32767
 
 	//	initialize previous weights to 0 for first iteration
 	for(i=1;i<numl;i++)
-		for(j=0;j<lsize[i];j++)
-			for(k=0;k<lsize[i-1]+1;k++)
+		for(int j=0;j<lsize[i];j++)
+			for(int k=0;k<lsize[i-1]+1;k++)
 				prevDwt[i][j][k]=(double)0.0;
 
-
 	stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-	printf("allocation complete.\n\n"); 
 
 	// Allocate device variables ----------------------------------------------
 
 	if(mode != CPU_NORMAL) {
 		printf("Allocating device variables..."); fflush(stdout);
 		startTime(&timer);
+
+		cuda_ret = cudaMalloc((void**)&data_d, (inputSize+outputSize) * sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&out_d, numl * sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&delta_d, numl * sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&weight_d, numl * sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&lsize_d, numl * sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&prevDwt, numl * sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&alpha_d, sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&beta_d, sizeof(double));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+		cuda_ret = cudaMalloc((void**)&numl_d, sizeof(int));
+		if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
 
 
 
@@ -147,6 +179,45 @@ int main(int argc, char* argv[])
 		printf("Copying data from host to device..."); fflush(stdout);
 		startTime(&timer);
 
+		cuda_ret = cudaMemcpy(data_d, data,
+				(inputSize+outputSize) * sizeof(double), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) FATAL("Unable to set device memory");
+
+		cuda_ret = cudaMemcpy(delta_d, delta,
+				numl * sizeof(double), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) {
+			FATAL("Unable to copy memory to the device");
+		}
+		cuda_ret = cudaMemcpy(weight_d, weight,
+				numl * sizeof(double), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) {
+			FATAL("Unable to copy memory to the device");
+		}
+		cuda_ret = cudaMemcpy(lsize_d, lsize,
+				numl * sizeof(double), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) {
+			FATAL("Unable to copy memory to the device");
+		}
+		
+
+		cuda_ret = cudaMemcpy(alpha_d,(double*)&alpha,
+				sizeof(double), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) {
+			FATAL("Unable to copy memory to the device");
+		}
+
+		cuda_ret = cudaMemcpy(beta_d, (double*) &beta,
+				sizeof(double), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) {
+			FATAL("Unable to copy memory to the device");
+		}
+
+		cuda_ret = cudaMemcpy(numl_d, (int*) &numl,
+				sizeof(int), cudaMemcpyHostToDevice);
+		if(cuda_ret != cudaSuccess) {
+			FATAL("Unable to copy memory to the device");
+		}
+
 		cudaDeviceSynchronize();
 		stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 	}
@@ -156,27 +227,45 @@ int main(int argc, char* argv[])
 	printf("Launching kernel ");
 
 	if(mode == CPU_NORMAL) {
-		printf("(CPU normal version)...");fflush(stdout);
+		printf("(CPU version)...");fflush(stdout);
 		startTime(&timer);
 
-		float mse;
-
-		for (i = 0; i < maxIterations; ++i)
+		printf("training the network...");
+		for (i=0; i<num_iter ; i++)
 		{
-			cpu_bpgt(&data[(i%dataPoints)*(inputSize+outputSize)],data[(i%dataPoints)*(inputSize+outputSize) + inputSize],
-					out, delta, weight, numl, lsize, beta, alpha, prevDwt);
+			cpu_bpgt(&data[(i%dataPoints)*(inputSize+outputSize)],
+					&data[(i%dataPoints)*(inputSize+outputSize) + inputSize],
+					out, delta,weight,&numl,lsize,&beta,&alpha,prevDwt);
 		}
 
 		stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 	} else if(mode == GPU_NAIVE) {
 		printf("(GPU naive version)...");fflush(stdout);
 		startTime(&timer);
+
+		printf("training the network...");
+		for (i=0; i<num_iter ; i++)
+		{
+			gpu_naive_bpgt(&data_d[(i%dataPoints)*(inputSize+outputSize)],
+					&data_d[(i%dataPoints)*(inputSize+outputSize) + inputSize],
+					out_d, delta_d,weight_d,numl_d,lsize_d,beta_d,alpha_d,prevDwt_d);
+		}
+
 		cuda_ret = cudaDeviceSynchronize();
 		if(cuda_ret != cudaSuccess) FATAL("Unable to launch/execute kernel");
 		stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 	} else if(mode == GPU_IMPROVED) {
-		printf("(GPU better)...");fflush(stdout);
+		printf("(GPU improved version)...");fflush(stdout);
 		startTime(&timer);
+
+		printf("training the network...");
+		for (i=0; i<num_iter ; i++)
+		{
+			gpu_improved_bpgt(&data[(i%dataPoints)*(inputSize+outputSize)],
+					&data[(i%dataPoints)*(inputSize+outputSize) + inputSize],
+					out_d, delta_d,weight_d,numl_d,lsize_d,beta_d,alpha_d,prevDwt_d);
+		}
+
 		cuda_ret = cudaDeviceSynchronize();
 		if(cuda_ret != cudaSuccess) FATAL("Unable to launch/execute kernel");
 		stopTime(&timer); printf("%f s\n", elapsedTime(timer));
@@ -185,13 +274,16 @@ int main(int argc, char* argv[])
 		exit(0);
 	}
 
-
 	// Copy device variables from host ----------------------------------------
 
 	if(mode != CPU_NORMAL) {
 
 		printf("Copying data from device to host..."); fflush(stdout);
 		startTime(&timer);
+
+		cuda_ret = cudaMemcpy(out, out_d, numl * sizeof(double),
+				cudaMemcpyDeviceToHost);
+		if(cuda_ret != cudaSuccess) FATAL("Unable to copy memory to host");
 
 		cudaDeviceSynchronize();
 		stopTime(&timer); printf("%f s\n", elapsedTime(timer));
@@ -200,17 +292,33 @@ int main(int argc, char* argv[])
 
 	// Verify correctness -----------------------------------------------------
 
-	printf("Verifying results..."); fflush(stdout);
+
+
+	if ( i == num_iter )
+		cout << endl << i << " iterations completed..." << endl;
+
+	cout<< "Now using the trained network to make predctions on test data...." << endl << endl;
+	for ( i = 0 ; i < testPoints ; ++i )
+	{
+		ffwd(&data[i*(inputSize+outputSize)],
+				out,weight,&numl,lsize);
+		for (int j=0; j < inputSize; ++j)
+		{
+			cout << data[i*(inputSize+outputSize)+j] << " ";
+		}
+		cout << "Ans:" << data[i*(inputSize+outputSize) + inputSize] <<  "  Guess:" << out[numl-1][0] << endl;
+	}
 
 	int prediction, actual;
-	float guess;
+	double guess;
 	int correct = 0, incorrect = 0;
 
-	for (i = 0; i < dataPoints; ++i)
+	for (int i = 0; i < dataPoints; ++i)
 	{
-		cpu_ffwd(&data[i*(inputSize+outputSize)], out, weight, numl, lsize);
-		actual = (int) data[i*(inputSize+outputSize) + inputSize];
-		guess = out[numl-1][0]; 
+		ffwd(&data[i*(inputSize+outputSize)],
+				out,weight,&numl,lsize);
+		actual = (int) (data[i*(inputSize+outputSize) + inputSize]);
+		guess = out[numl-1][0];
 		prediction = (int)(guess + 0.5);
 		if (prediction == actual)
 		{
@@ -220,49 +328,55 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	for ( i = 0 ; i < dataPoints ; ++i )
-	{
-		cpu_ffwd(&data[i*(inputSize+outputSize)], out, weight, numl, lsize);
-		for (j=0; j < inputSize; ++j)
-		{
-			printf("%f ", data[i*(inputSize+outputSize)+j]);
-		}
-		printf("Ans: %f Guess: %f\n", data[i*(inputSize+outputSize) + inputSize],  out[numl-1][0]);
-	}
+	double accuracy = ((double)correct)/((double)(correct+incorrect));
 
-	float accuracy = ((float)correct)/((float)(correct+incorrect));
-	printf("%.2f%% accurate.\n", accuracy * 100);
+	cout << "Correct = " << correct << endl;
+	cout << "Incorrect = " << incorrect << endl;
+	cout << "Accuracy = " << accuracy*100 << "%" << endl;
+
+
+	//	free out
+	for(i=0;i<numl;i++)
+		delete[] out[i];
+	delete[] out;
+
+	//	free delta
+	for(i=1;i<numl;i++)
+		delete[] delta[i];
+	delete[] delta;
+
+	//	free weight
+	for(i=1;i<numl;i++)
+		for(int j=0;j<lsize[i];j++)
+			delete[] weight[i][j];
+	for(i=1;i<numl;i++)
+		delete[] weight[i];
+	delete[] weight;
+
+	//	free prevDwt
+	for(i=1;i<numl;i++)
+		for(int j=0;j<lsize[i];j++)
+			delete[] prevDwt[i][j];
+	for(i=1;i<numl;i++)
+		delete[] prevDwt[i];
+	delete[] prevDwt;
+
+	//	free layer info
+	delete[] lsize;
 
 	// Free memory ------------------------------------------------------------
 
-	free(lsize);
-
-	for( i=0;i<numl;i++){
-		free(out[i]);
+	if(mode != CPU_NORMAL) {
+		cudaFree(data_d);
+		cudaFree(out_d);
+		cudaFree(delta_d);
+		cudaFree(weight_d);
+		cudaFree(lsize_d);
+		cudaFree(prevDwt_d);
+		cudaFree(alpha_d);
+		cudaFree(beta_d);
+		cudaFree(numl_d);
 	}
-	free(out);
-
-	for(i=1;i<numl;i++){
-		free(delta[i]);
-	}
-	free(delta);
-
-	for(i=1;i<numl;i++){
-		for(j=0;j<lsize[i];j++){
-			free(weight[i][j]);
-		}
-		free(weight[i]);
-	}
-	free(weight);
-
-	for(i=1;i<numl;i++){
-		for(j=0;j<lsize[i];j++){
-			free(prevDwt[i][j]);
-		}
-		free(prevDwt[i]);
-	}
-	free(prevDwt);
 
 	return 0;
 }
-
