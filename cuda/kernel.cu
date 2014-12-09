826f7ea62b5af18d421c7f3638406b4ced5c0ae2
kyle921 <kroes921@gmail.com>
2014-12-09 15:20:31 -0600

    Update GPU naive kernel for flat matrices and fix loop nesting

--- cuda/kernel.cu
+++ cuda/kernel.cu
@@ -5,102 +5,108 @@
   GPU main computation kernels
  *******************************************************************************/
 
-__global__ void gpu_naive_kernel(float *in,float *tgt,
-		float *out,
-		float *delta,
+__global__ void gpu_naive_kernel(double *in,double *tgt,
+		double *out,
+		double *delta,
 		int *rowptr_od,
-		float *weight,
+		double *weight,
 		int numl,
 		int *lsize,
-		float beta,
-		float alpha,
-		float *prevDwt,
+		double beta,
+		double alpha,
+		double *prevDwt,
 		int *rowptr_w) {
 
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
+	int sz =0;
+	double sum;
 
+	// update output values for each neuron
 
-	float sum;
-
-	//	update output values for each neuron
-
-
-	//	assign content to input layer
+	// assign content to input layer
 	if (idx < lsize[0])
 	{
-		out[0][idx]=in[idx];  // output_from_neuron(i,j) Jth neuron in Ith Layer
+		out[idx]=in[idx];  // output_from_neuron(i,j) Jth neuron in Ith Layer
 	}
-
 
 	__syncthreads();
 
-	//	assign output(activation) value 
-	//	to each neuron usng sigmoid func
-	if (idx < *numl) {
-		for(int j=0;j<lsize[idx];j++){		// For each neuron in current layer
-			sum=0.0;
-			for(int k=0;k<lsize[idx-1];k++){		// For input from each neuron in preceeding layer
-				sum+= out[idx-1][k]*weight[idx][j][k];	// Apply weight to inputs and add to sum
+	// assign output(activation) value
+	// to each neuron usng sigmoid func
+	for(int i=1;i<numl;i++){
+		sum=0.0;
+		if (idx < lsize[i]){
+			for(int k=0;k<lsize[i-1];k++){ // For input from each neuron in preceeding layer
+				// Apply weight to inputs and add to sum
+				sum+= out[rowptr_od[i-1]+k]* weight[sz+(idx*(lsize[i-1]+1))+k];
 			}
-			sum+=weight[idx][j][lsize[idx-1]];		// Apply bias
-			out[idx][j]=(float)(1/(1+exp(-sum)));
+			sum+= weight[sz+lsize[i-1]*idx + lsize[i-1]];
+			out[rowptr_od[i]+idx]=(double)(1/(1+exp(-sum)));
+		}
+		sz+= (lsize[i-1]+1)*lsize[i];
+		__syncthreads();
+	}
+
+	__syncthreads();
+
+	// find delta for output layer
+	if (idx == 0){
+		for(int i=0;i<lsize[numl-1];i++){
+			delta[rowptr_od[numl-1]+i]=out[rowptr_od[numl-1]+i]*(1-out[rowptr_od[numl-1]+i])*(tgt[i]-out[rowptr_od[numl-1]+i]);
 		}
 	}
 
 	__syncthreads();
 
-	//	find delta for output layer
-	if (idx < lsize[*numl-1]) {
-		delta[(*numl)-1][idx]=out[(*numl)-1][idx]*(1-out[(*numl)-1][idx])*(tgt[idx]-out[(*numl)-1][idx]);
-	}
-	__syncthreads();
-
-	//	find delta for hidden layers	
-	if (idx < *numl-1 && idx > 0) {
-		for(int j=0;j<lsize[idx];j++){
-			sum=0.0;
-			for(int k=0;k<lsize[idx+1];k++){
-				sum+=delta[idx+1][k]*weight[idx+1][k][j];
+	//	find delta for hidden layers
+	for(int i=numl-2;i>0;i--){
+		sum=0.0;
+		if (idx<lsize[i]){
+			for(int k=0;k<lsize[i+1];k++){
+				sum+=delta[rowptr_od[i+1]+k]*weight[rowptr_w[i+1]+k*(lsize[i]+1)+idx];   // look into // rowptr_WD starts from 1
 			}
-			delta[idx][j]=out[idx][j]*(1-out[idx][j])*sum;
+			delta[rowptr_od[i]+idx]=out[rowptr_od[i]+idx]*(1-out[rowptr_od[i]+idx])*sum;
 		}
+		__syncthreads();
 	}
+
 	__syncthreads();
 
 	//	apply momentum ( does nothing if alpha=0 )
-	if (idx < *numl && idx > 0) {
-		for(int j=0;j<lsize[idx];j++){
-			for(int k=0;k<lsize[idx-1];k++){
-				weight[idx][j][k]+=(*alpha)*prevDwt[idx][j][k];
+	for(int i=1;i<numl;i++){
+			if(idx<lsize[i]){
+			for(int k=0;k<lsize[i-1];k++){
+				weight[rowptr_w[i]+idx*(lsize[i-1]+1)+k]+=alpha*prevDwt[rowptr_w[i]+idx*(lsize[i-1]+1)+k];
 			}
-			weight[idx][j][lsize[idx-1]]+=(*alpha)*prevDwt[idx][j][lsize[idx-1]];
+			weight[rowptr_w[i]+idx*(lsize[i-1]+1)+lsize[i-1]]+=alpha*prevDwt[rowptr_w[i]+idx*(lsize[i-1]+1)+lsize[i-1]];
 		}
+		__syncthreads();
 	}
 	__syncthreads();
 
-	//	adjust weights usng steepest descent	
-	if (idx < *numl && idx > 0) {
-		for(int j=0;j<lsize[idx];j++){
-			for(int k=0;k<lsize[idx-1];k++){
-				prevDwt[idx][j][k]=(*beta)*delta[idx][j]*out[idx-1][k];
-				weight[idx][j][k]+=prevDwt[idx][j][k];
+	//	adjust weights usng steepest descent
+	for(int i=1;i<numl;i++){
+		if(idx<lsize[i]){
+			for(int k=0;k<lsize[i-1];k++){
+				prevDwt[rowptr_w[i]+idx*(lsize[i-1]+1)+k]=beta*delta[rowptr_od[i]+idx]*out[rowptr_od[i-1]+k];
+				weight[rowptr_w[i]+idx*(lsize[i-1]+1)+k]+=prevDwt[rowptr_w[i]+idx*(lsize[i-1]+1)+k];
 			}
-			prevDwt[idx][j][lsize[idx-1]]=(*beta)*delta[idx][j];
-			weight[idx][j][lsize[idx-1]]+=prevDwt[idx][j][lsize[idx-1]];
+			prevDwt[rowptr_w[i]+idx*(lsize[i-1]+1)+lsize[i-1]]=beta*delta[rowptr_od[i]+idx];
+			weight[rowptr_w[i]+idx*(lsize[i-1]+1)+lsize[i-1]]+=prevDwt[rowptr_w[i]+idx*(lsize[i-1]+1)+lsize[i-1]];
 		}
 	}
 }
 
-__global__ void gpu_improved_kernel(float *in,float *tgt,
-		float *out,
-		float *delta,
+__global__ void gpu_improved_kernel(double *in,double *tgt,
+		double *out,
+		double *delta,
 		int *rowptr_od,
-		float *weight,
+		double *weight,
 		int numl,
 		int *lsize,
-		float beta,
-		float alpha,
-		float *prevDwt,
+		double beta,
+		double alpha,
+		double *prevDwt,
 		int *rowptr_w) {
 
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
@@ -113,155 +119,146 @@ __global__ void gpu_improved_kernel(float *in,float *tgt,
   Main computation functions
  *******************************************************************************/
 
-void gpu_naive_bpgt(float *in,float *tgt,
-		float *out,
-		float *delta,
+void gpu_naive_bpgt(double *in,double *tgt,
+		double *out,
+		double *delta,
 		int *rowptr_od,
-		float *weight,
+		double *weight,
 		int numl,
 		int *lsize,
-		float beta,
-		float alpha,
-		float *prevDwt,
+		double beta,
+		double alpha,
+		double *prevDwt,
 		int *rowptr_w) {
 
 	const unsigned int numThreadsPerBlock = 512;
 	const unsigned int numBlocks = (128 - 1)/numThreadsPerBlock + 1;
 	gpu_naive_kernel <<< numBlocks , numThreadsPerBlock >>>
-		(in,tgt,out,delta,weight,numl,lsize,beta,alpha,prevDwt);
+		(in,tgt,out,delta,rowptr_od,weight,numl,lsize,beta,alpha,prevDwt, rowptr_w);
 }
 
-void gpu_improved_bpgt(float *in,float *tgt,
-		float *out,
-		float *delta,
+void gpu_improved_bpgt(double *in,double *tgt,
+		double *out,
+		double *delta,
 		int *rowptr_od,
-		float *weight,
+		double *weight,
 		int numl,
 		int *lsize,
-		float beta,
-		float alpha,
-		float *prevDwt,
+		double beta,
+		double alpha,
+		double *prevDwt,
 		int *rowptr_w) {
 
 	const unsigned int numThreadsPerBlock = 512;
 	const unsigned int numBlocks = (128 - 1)/numThreadsPerBlock + 1;
 	gpu_improved_kernel <<< numBlocks , numThreadsPerBlock >>>
-		(in,tgt,out,delta,weight,numl,lsize,beta,alpha,prevDwt);
+		(in,tgt,out,delta,rowptr_od,weight,numl,lsize,beta,alpha,prevDwt, rowptr_w);
 
 }
 
 
 
-//	backpropogate errors from output
-//	layer uptill the first hidden layer
-void cpu_bpgt(float *in,float *tgt,
-		float *out,
-		float *delta,
+
+
+void cpu_bpgt(double *in,double *tgt,
+		double *out,
+		double *delta,
 		int *rowptr_od,
-		float *weight,
+		double *weight,
 		int numl,
 		int *lsize,
-		float beta,
-		float alpha,
-		float *prevDwt,
+		double beta,
+		double alpha,
+		double *prevDwt,
 		int *rowptr_w)
 {
-	float sum;
+	double sum;
 	int i;
 
-	//	update output values for each neuron
-
-
-	//	assign content to input layer
 	for(i=0;i<lsize[0];i++)
 	{
-		out[0][i]=in[i];  // output_from_neuron(i,j) Jth neuron in Ith Layer
+		out[rowptr_od[0]+i]=in[i];  
 	}
-
-	//	assign output(activation) value 
-	//	to each neuron usng sigmoid func
-	for(i=1;i<*numl;i++){				// For each layer
-		for(int j=0;j<lsize[i];j++){		// For each neuron in current layer
+	
+	for(i=1;i<numl;i++){				
+		for(int j=0;j<lsize[i];j++){		
 			sum=0.0;
-			for(int k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
-				sum+= out[i-1][k]*weight[i][j][k];	// Apply weight to inputs and add to sum
+			for(int k=0;k<lsize[i-1];k++){		
+				sum+= out[rowptr_od[i-1]+k]*weight[rowptr_w[i] + (lsize[i-1]+1)*j+k];	
 			}
-			sum+=weight[i][j][lsize[i-1]];		// Apply bias
-			out[i][j]=(float)(1/(1+exp(-sum)));
+			sum+=weight[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]];		
+			out[rowptr_od[i]+j]=(double)(1/(1+exp(-sum)));
 		}
 	}
 
-
-	//	find delta for output layer
-	for(i=0;i<lsize[(*numl)-1];i++){
-		delta[(*numl)-1][i]=out[(*numl)-1][i]*
-			(1-out[(*numl)-1][i])*(tgt[i]-out[(*numl)-1][i]);
+	
+	for(i=0;i<lsize[(numl)-1];i++){
+		delta[rowptr_od[(numl)-1]+i]=out[rowptr_od[(numl)-1]+i]*
+			(1-out[rowptr_od[(numl)-1]+i])*(tgt[i]-out[rowptr_od[(numl)-1]+i]);
 	}
 
-	//	find delta for hidden layers	
-	for(i=*numl-2;i>0;i--){
+	for(i=numl-2;i>0;i--){
 		for(int j=0;j<lsize[i];j++){
 			sum=0.0;
 			for(int k=0;k<lsize[i+1];k++){
-				sum+=delta[i+1][k]*weight[i+1][k][j];
+				sum+=delta[rowptr_od[i+1]+k]*weight[rowptr_w[i+1]+(lsize[i]+1)*k+j];
 			}
-			delta[i][j]=out[i][j]*(1-out[i][j])*sum;
+			delta[rowptr_od[i]+j]=out[rowptr_od[i]+j]*(1-out[rowptr_od[i]+j])*sum;
 		}
 	}
-
-	//	apply momentum ( does nothing if alpha=0 )
-	for(i=1;i<*numl;i++){
+	
+	for(i=1;i<numl;i++){
 		for(int j=0;j<lsize[i];j++){
 			for(int k=0;k<lsize[i-1];k++){
-				weight[i][j][k]+=(*alpha)*prevDwt[i][j][k];
+				weight[rowptr_w[i] + (lsize[i-1]+1)*j+k]+=(alpha)*prevDwt[rowptr_w[i] + (lsize[i-1]+1)*j+k];
 			}
-			weight[i][j][lsize[i-1]]+=(*alpha)*prevDwt[i][j][lsize[i-1]];
+			weight[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]]+=(alpha)*prevDwt[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]];
 		}
 	}
-
-	//	adjust weights usng steepest descent	
-	for(i=1;i<*numl;i++){
+	
+	for(i=1;i<numl;i++){
 		for(int j=0;j<lsize[i];j++){
 			for(int k=0;k<lsize[i-1];k++){
-				prevDwt[i][j][k]=(*beta)*delta[i][j]*out[i-1][k];
-				weight[i][j][k]+=prevDwt[i][j][k];
+				prevDwt[rowptr_w[i] + (lsize[i-1]+1)*j+k]=(beta)*delta[rowptr_od[i]+j]*out[rowptr_od[i-1]+k];
+				weight[rowptr_w[i] + (lsize[i-1]+1)*j+k]+=prevDwt[rowptr_w[i] + (lsize[i-1]+1)*j+k];
 			}
-			prevDwt[i][j][lsize[i-1]]=(*beta)*delta[i][j];
-			weight[i][j][lsize[i-1]]+=prevDwt[i][j][lsize[i-1]];
+			prevDwt[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]]=(beta)*delta[rowptr_od[i]+j];
+			weight[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]]+=prevDwt[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]];
 		}
 	}
 }
 
 
-// feed forward one set of input
-void ffwd(float *in,
-		float *out,
-		float *weight,
+
+void ffwd(double *in,
+		double *out,
+		double *weight,
 		int numl,
-		int *lsize)
+		int *lsize,
+		int *rowptr_od,
+		int *rowptr_w)
 {
-	float sum;
+	double sum;
 	int i=0;
 
-	//	assign content to input layer
+	
 	for(i=0;i<lsize[0];i++)
 	{
-		out[0][i]=in[i];  // output_from_neuron(i,j) Jth neuron in Ith Layer
+		out[rowptr_od[0]+i]=in[i];  
 	}
 
-	//	assign output(activation) value 
-	//	to each neuron usng sigmoid func
-	for(i=1;i<*numl;i++){				// For each layer
-		for(int j=0;j<lsize[i];j++){		// For each neuron in current layer
+	
+	
+	for(i=1;i<numl;i++){				
+		for(int j=0;j<lsize[i];j++){		
 			sum=0.0;
-			for(int k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
-				sum+= out[i-1][k]*weight[i][j][k];	// Apply weight to inputs and add to sum
+			for(int k=0;k<lsize[i-1];k++){		
+				sum+= out[rowptr_od[i-1]+k]*weight[rowptr_w[i] + (lsize[i-1]+1)*j+k];	
 			}
-			sum+=weight[i][j][lsize[i-1]];		// Apply bias
-			out[i][j]=(float)(1/(1+exp(-sum)));
+			sum+=weight[rowptr_w[i] + (lsize[i-1]+1)*j+lsize[i-1]];		
+			out[rowptr_od[i]+j]=(double)(1/(1+exp(-sum)));
 		}
 	}
 }
-
 
 
